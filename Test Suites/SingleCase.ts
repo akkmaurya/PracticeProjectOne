2a9ec94ec21fddd41a692de5b99c5b84cdaf6391
AkashMaurya <AkashMaurya@CWERX-0668>
2024-09-20 16:48:36 +0530

    Third Commit

---
+++ Test Suites/SingleCase.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SingleCase</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3dd872d9-554d-4203-bc94-a5df88c28be8</testSuiteGuid>
+   <testCaseLink>
+      <guid>5413159b-8332-4246-9705-1bf5a34febf1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LaunchPetStorePage</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
