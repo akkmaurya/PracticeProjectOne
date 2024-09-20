bb028b55b2d659f50eb3aef8600d77cdbfa29f44
AkashMaurya <AkashMaurya@CWERX-0668>
2024-09-20 15:11:20 +0530

    First Commit

---
+++ Scripts/LaunchPetStorePage/Script1726825174508.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+/*
+* 1. Open Browser
+* 2. Navigate URL https://petstore.octoperf.com/actions/Catalog.action
+* 3. Verify Sign In link present on page 
+*/
+// Open Browser
+WebUI.openBrowser('')
+
+// Navigate to URL
+WebUI.navigateToUrl('https://petstore.octoperf.com/actions/Catalog.action')
+
+// Verify Sign In link present on page
+WebUI.verifyElementPresent(findTestObject('Page_Catalog/a_Sign In'))
